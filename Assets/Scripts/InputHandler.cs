4814868ce4f763c87b34a89c6e2bb1a90c55dcb3
John Karasinski <karasinski@gmail.com>
2017-08-17 21:22:12 -0700

    add time delay

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class InputHandler : MonoBehaviour
 {
-    public float speed;
+    public float speed, delay;
     private List<int> rotations = new List<int>(new int[] { 45, -45, 0, 90, -90 });
     private Vector3 zero = new Vector3(0, 0, 5);
     private Quaternion rotation;
+    private Vector3 delta;
 
     public GameObject Target, Control;
 
-    public static Vector3 RandomPointOnUnitCircle(float radius)
+    private static Vector3 RandomPointOnUnitCircle(float radius)
     {
         float angle = Random.Range(0f, Mathf.PI * 2);
         float x = Mathf.Sin(angle) * radius;
@@ -20,19 +21,30 @@ public class InputHandler : MonoBehaviour
         return new Vector3(x, y, 0);
     }
 
-    void setRotation()
+    private void setRotation()
     {
-        rotation = Quaternion.Euler(0, 0, rotations[Random.Range(0, rotations.Count)]);
-        print(rotation);
+        int choice = rotations[Random.Range(0, rotations.Count)];
+        rotation = Quaternion.Euler(0, 0, choice);
+        print(choice);
     }
 
-    void Update()
+    private IEnumerator delayedInput(Vector3 delta)
     {
-        Vector3 delta = rotation * new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"), 0.0f) * Time.deltaTime * speed;
+        if (delay > 0)
+        {
+            yield return new WaitForSeconds(delay);
+        }
+
         Control.transform.position += delta;
     }
 
-    void OnTriggerEnter(Collider other)
+    private void Update()
+    {
+        delta = rotation * new Vector3(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"), 0.0f) * Time.deltaTime * speed;
+        StartCoroutine(delayedInput(delta));
+    }
+
+    private void OnTriggerEnter(Collider other)
     {
         Control.transform.position = zero;
         Target.transform.position = zero + RandomPointOnUnitCircle(10);
